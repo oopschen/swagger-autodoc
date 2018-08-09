dd6d94343d6a0fc834612736b8a87f6544b644fb
Ray <linxray@gmail.com>
2018-08-09 16:05:47 +0800

    add script release and start

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,9 +1,33 @@
-import 'path';
+import webpack from 'webpack';
+import wmerge from 'webpack-merge';
+
 import baseconfig from './webpack.config.base.js';
 
-let createConfig = config => ({
-    ...config,
-    name: "base"
-});
 
-export default createConfig(baseconfig);
+const DEV_CFG = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
+
+const PROD_CFG = {
+  mode: 'production'
+
+};
+
+let chooseCfg = () => {
+  if  ("production" === process.env.NODE_ENV) {
+    return PROD_CFG;
+  }
+
+  return DEV_CFG;
+};
+
+export default wmerge(baseconfig, chooseCfg());
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,5 +1,24 @@
+import path from 'path';
+
+
 export default ({
-  entry: {
-    "index": "./src/script/index.js"
+  entry: () => './src/script/entry',
+  output: {
+    chunkFilename: '[name].[chunkhash].c.js',
+    chunkLoadTimeout: 5000,
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 });
